ae710919b0fa1ea3d7b114e6c5f64ffc434b3634
bs-jberjawi <jaafar.berjawi@buildsoft.com.au>
2026-02-22 03:12:16 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://fbhyotodrwciaavihfza.supabase.co',
   supabaseAnonKey: 'sb_publishable_GugoDZU8a_YY2ZvmPH5DuA_LqtwGRtq',
-  geminiApiKey: 'AIzaSyD6oqvSa1XjyLtIjLM6_vKxdtSk3xVZHQA',
+  geminiApiKey: '',
 };
